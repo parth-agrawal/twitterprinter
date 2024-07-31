959407325a4dc975e8a2afa03c04c4e02486a519
Parth Agrawal <pvagrawal0@gmail.com>
2024-07-31 17:00:13 -0400

    full stack is setup

---
+++ backend/app.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+	cors({
+		origin: ['origin(s)'],
+		allowedHeaders: ['Authorization', 'Content-Type'],
+	})
+);
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
+
+
+
+
+export default app;
---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+
+export default function App() {
+  return (
+    <header>
+      Test
+    </header>
+  )
+}
